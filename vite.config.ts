d4e4034c705431de0b355f50c4858f87d2d8d1ce
none <none>
2023-03-21 13:55:20 +0800

    gzip压缩

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import gzipPlugin from "rollup-plugin-gzip";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      plugins: [gzipPlugin()],
+    },
+  }
+});
