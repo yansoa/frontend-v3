71f3687061dcccde29661b688dcf5dc5d8cf2848
禺狨 <18119604035@163.com>
2023-04-28 17:32:50 +0800

    收藏习题文案优化

--- src/pages/exam/collection/index.tsx
+++ src/pages/exam/collection/index.tsx
@@ -78,37 +78,37 @@ export const ExamCollectionPage = () => {
         {list[1] > 0 && (
           <div className={styles["question-item"]} onClick={() => goDetail(1)}>
             <div className={styles["question-item-type"]}>单选题</div>
-            <div className={styles["question-item-num"]}>共{list[1]}题错题</div>
+            <div className={styles["question-item-num"]}>已收藏{list[1]}题</div>
           </div>
         )}
         {list[2] > 0 && (
           <div className={styles["question-item"]} onClick={() => goDetail(2)}>
             <div className={styles["question-item-type"]}>多选题</div>
-            <div className={styles["question-item-num"]}>共{list[2]}题错题</div>
+            <div className={styles["question-item-num"]}>已收藏{list[2]}题</div>
           </div>
         )}
         {list[5] > 0 && (
           <div className={styles["question-item"]} onClick={() => goDetail(5)}>
             <div className={styles["question-item-type"]}>判断题</div>
-            <div className={styles["question-item-num"]}>共{list[5]}题错题</div>
+            <div className={styles["question-item-num"]}>已收藏{list[5]}题</div>
           </div>
         )}
         {list[3] > 0 && (
           <div className={styles["question-item"]} onClick={() => goDetail(3)}>
             <div className={styles["question-item-type"]}>填空题</div>
-            <div className={styles["question-item-num"]}>共{list[3]}题错题</div>
+            <div className={styles["question-item-num"]}>已收藏{list[3]}题</div>
           </div>
         )}
         {list[4] > 0 && (
           <div className={styles["question-item"]} onClick={() => goDetail(4)}>
             <div className={styles["question-item-type"]}>问答题</div>
-            <div className={styles["question-item-num"]}>共{list[4]}题错题</div>
+            <div className={styles["question-item-num"]}>已收藏{list[4]}题</div>
           </div>
         )}
         {list[6] > 0 && (
           <div className={styles["question-item"]} onClick={() => goDetail(6)}>
             <div className={styles["question-item-type"]}>题帽题</div>
-            <div className={styles["question-item-num"]}>共{list[1]}题错题</div>
+            <div className={styles["question-item-num"]}>已收藏{list[1]}题</div>
           </div>
         )}
       </div>
