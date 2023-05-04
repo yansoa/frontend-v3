f0c7018018b0040d0777ae08796d4781c87da70e
none <none>
2023-05-04 16:26:45 +0800

    == => ===

--- src/js/config.ts
+++ src/js/config.ts
@@ -1,4 +1,4 @@
 export default {
   app_url: import.meta.env.VITE_APP_URL || "",
-  disable_vip: (import.meta.env.VITE_DISABLE_VIP || "") == "disable",
+  disable_vip: (import.meta.env.VITE_DISABLE_VIP || "") === "disable",
 };
